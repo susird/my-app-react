dcd51acdb92a09557346dd3ada8154d31b7905ff
susana <susir95@gmail.com>
2021-07-27 02:48:33 -0500

    fixing filters

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -5,8 +5,8 @@ import './styles.css';
 export function Filters () {
   return (
     <div className="filters">
-      <Link to="/all" className="filter-all">All  •</Link> 
-      <Link to="/active" className="filter-active">Active  •</Link>
+      <Link to="/all" className="filter-all">All</Link> 
+      <Link to="/active" className="filter-active">Active</Link>
       <Link to="/completed" className="filter-completed">Completed</Link>
     </div>
   )
